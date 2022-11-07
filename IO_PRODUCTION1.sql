5cd566683e6fdf2581c97a6ac66b6ea00072048f
Rudra Dutt <64432459+Rudra028@users.noreply.github.com>
2022-11-08 01:30:04 +0530

    Delete IO_PRODUCTION1.sql

--- IO_PRODUCTION1.sql
+++
@@ -1,5 +0,0 @@
-alter session set current_schema = apps;
-
-select * from rgis_hr_person_v where first_name like '%Brenda%' and last_name like '%Sandavol%' ;
-
-select * from rgis_hr_person_v where first_name like '%Clarissa%' and last_name like '%Brown%' ;
